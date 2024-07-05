ed232e46e60c2621cbdae8f779231bfb57bd8333
Prachi Bhatt <bprachi443@gmail.com>
2024-07-05 22:20:42 +0530

    db connected

--- prisma/migrations/20240705082242_init/migration.sql
+++ prisma/migrations/20240705082242_init/migration.sql
@@ -1,4 +1,15 @@
 -- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `bonus` INTEGER NOT NULL DEFAULT 0,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `Referral` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `referrerName` VARCHAR(191) NOT NULL,
@@ -6,6 +17,8 @@ CREATE TABLE `Referral` (
     `refereeName` VARCHAR(191) NOT NULL,
     `refereeEmail` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+    `purchaseAmount` INT,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -18,16 +31,8 @@ CREATE TABLE `Purchase` (
     `courseId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    PRIMARY KEY (`id`),
 
--- CreateTable
-CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
-    `bonus` INTEGER NOT NULL DEFAULT 0,
-
-    UNIQUE INDEX `User_email_key`(`email`),
-    PRIMARY KEY (`id`)
+    FOREIGN KEY (`referrerId`) REFERENCES `User`(`id`),
+    FOREIGN KEY (`refereeId`) REFERENCES `User`(`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20240705111557_add_status_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `referral` ADD COLUMN `status` VARCHAR(191) NOT NULL DEFAULT 'pending';
